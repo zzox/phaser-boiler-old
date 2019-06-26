e1ac1ac2b97a5d2d9a54b31fd616c60e82ef8bf8
zzox <tweedlitweedli@gmail.com>
2019-06-25 23:08:36 -0500

    2019 update

--- config/webpack.development.config.js
+++ config/webpack.development.config.js
@@ -1,22 +1,26 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const { phaser, phaserModule, nodeModules, dist, dev } = require('./paths');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-//   .BundleAnalyzerPlugin;
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+const path = require('path')
+const join = (dir) => path.join(__dirname, dir)
+const nodeModules = join('../node_modules/')
+const dev = join('../dev')
+const dist = join('../dist')
 
 // https://webpack.js.org/plugins/define-plugin/
 const definePlugin = new webpack.DefinePlugin({
   // set vars needed by phaser
   'typeof SHADER_REQUIRE': JSON.stringify(false),
-  'typeof CANVAS_RENDERER': JSON.stringify(true),
+  'typeof CANVAS_RENDERER': JSON.stringify(false),
   'typeof WEBGL_RENDERER': JSON.stringify(true)
-});
+})
 
 // https://github.com/jantimon/html-webpack-plugin
 const htmlPlugin = new HtmlWebpackPlugin({
   template: './index.html'
-});
+})
 
 // https://www.npmjs.com/package/browser-sync-webpack-plugin
 const browserSyncPlugin = new BrowserSyncPlugin(
@@ -26,72 +30,70 @@ const browserSyncPlugin = new BrowserSyncPlugin(
     proxy: 'http://localhost:8080/'
   },
   { reload: false } // stop BrowserSync reloading page, leave it to Webpack Dev Server
-);
+)
 
 // https://www.npmjs.com/package/webpack-bundle-analyzer
 // const analyzerPlugin = new BundleAnalyzerPlugin();
 
-module.exports = (env, options) => {
-  return {
-    devServer: {
-      host: '0.0.0.0' // allows external access to host (e.g. for testing on mobile)
-    },
-    mode: 'development', // dictates webpack defaults (development = faster build, production = smaller filesize)
-    output: {
-      path: dist,
-      filename: '[name].bundle.js',
-      chunkFilename: '[name].bundle.js',
-      publicPath: '/'
-    },
-    optimization: {
-      splitChunks: {
-        chunks: 'all' // separates vendor bundles from main
-      }
-    },
-    plugins: [definePlugin, htmlPlugin, browserSyncPlugin],
-    module: {
-      rules: [
-        { // transpile js using babel
-          test: /\.js$/,
-          exclude: [nodeModules],
-          use: {
-            loader: 'babel-loader'
-          }
-        },
-        { // loader for media files https://webpack.js.org/loaders/file-loader/
-          test: /\.(png|jpg|gif|ico|svg|pvr|pkm|static|ogg|mp3|wav)$/,
-          exclude: [nodeModules],
-          use: ['file-loader'] 
-        },
-        { // loader to import files as a string https://webpack.js.org/loaders/raw-loader/
-          test: [/\.vert$/, /\.frag$/],
-          exclude: [nodeModules],
-          use: 'raw-loader' 
-        },
-        { // load css as part of js bundle saving a request
-          test: /\.css$/,
-          use: [
-            {
-              loader: 'style-loader'
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 1,
-                localIdentName: '[name]_[local]_[hash:base64]',
-                sourceMap: true,
-                minimize: true
-              }
-            }
-          ]
-        }
-      ]
-    },
-    node: {
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty'
+module.exports = (env, options) => ({
+  devServer: {
+    host: '0.0.0.0' // allows external access to host (e.g. for testing on mobile)
+  },
+  mode: 'development', // dictates webpack defaults (development = faster build, production = smaller filesize)
+  output: {
+    path: dist,
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    publicPath: '/'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all' // separates vendor bundles from main
     }
-  };
-};
+  },
+  plugins: [definePlugin, htmlPlugin, browserSyncPlugin],
+  module: {
+    rules: [
+      { // transpile js using babel
+        test: /\.js$/,
+        exclude: [nodeModules],
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      { // loader for media files https://webpack.js.org/loaders/file-loader/
+        test: /\.(png|jpg|gif|ico|svg|pvr|pkm|static|ogg|mp3|wav)$/,
+        exclude: [nodeModules],
+        use: ['file-loader'] 
+      },
+      { // loader to import files as a string https://webpack.js.org/loaders/raw-loader/
+        test: [/\.vert$/, /\.frag$/],
+        exclude: [nodeModules],
+        use: 'raw-loader' 
+      },
+      { // load css as part of js bundle saving a request
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = (env, options) => {
-  const {mode} = options;
+  const { mode } = options
+
   return mode === 'development'
     ? require('./config/webpack.development.config.js')(env, options)
-    : require('./config/webpack.production.config.js')(env, options);
-};
+    : require('./config/webpack.production.config.js')(env, options)
+}
